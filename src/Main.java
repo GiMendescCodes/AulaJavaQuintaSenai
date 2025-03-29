d3e3e67e3467dfbdb38510356cb1b35d1dd7063e
GiMendescCodes <giovannamendessenai@gmail.com>
2025-03-29 09:40:56 -0300

    Teste commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    int num = 10;
+    int num1 = 10;
+
+        System.out.println(num+num1);
+
+        String InputSenaiJava = JOptionPane.showInputDialog("??");
+
+        int input = Integer.parseInt(InputSenaiJava);
+        input++;
+
+        String InputSenaiJava = JOptionPane.showInputDialog("??");
+    }
+}
